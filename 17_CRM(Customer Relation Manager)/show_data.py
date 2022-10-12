6301086315e80010718e0f798fe204918cef2c41
Yatora <anonymusgyrex@gmail.com>
2022-10-12 22:04:02 +0700

    import database to excel

--- 17_CRM(Customer Relation Manager)/show_data.py
+++ 17_CRM(Customer Relation Manager)/show_data.py
@@ -1,7 +1,6 @@
-from logging import root
 from tkinter import *
 from tkinter.font import BOLD
-from turtle import width
+import csv
 
 
 def show_data_window(data:list):
@@ -10,6 +9,12 @@ def show_data_window(data:list):
     icon_app = PhotoImage(file="./source/favicon/customer.png")
     rootShowData.tk.call("wm","iconphoto", rootShowData._w, icon_app)
     rootShowData.geometry("1380x500")
+
+    def event_save_csv(data_list):
+        with open('customer.csv','a', newline="") as f:
+            c = csv.writer(f, dialect="excel")
+            for i in data_list:
+                c.writerow(i)
     #title
     title_label = Label(rootShowData, text="DATA LIST", font=("verdana", 15, BOLD))
     title_label.grid(row=0, column=0, sticky=W+E)
@@ -28,13 +33,18 @@ def show_data_window(data:list):
         entry_data_list_header.grid(row=0,column=x,sticky=W+E)
         x+=1
     #data_list
-    
+    data_list = []
     for j in range(len(data)):
         c_data = list(data[j])
+        data_list.append(c_data)
         c_data.remove(c_data[-1])
         for k in range(len(c_data)):    
             entry_data_list = Entry(frame_list_data,font=("verdana",6))
             entry_data_list.insert(0,c_data[k])
             entry_data_list.grid(row=j+y, column=k)
-            
+    
+    
+    #button save
+    btn_save = Button(rootShowData, text="save to CVS", command=lambda:event_save_csv(data_list))
+    btn_save.grid(row=3,column=0, padx=10, sticky=W)
     rootShowData.mainloop()
